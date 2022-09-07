6bb379946a93708bb0c0b50c57170ff2e11f50c3
Camilo Hernández Ruiz <camihruiz24@gmail.com>
2022-09-07 15:27:36 -0500

    Add files via upload

---
+++ MyChatRoom.py
@@ -0,0 +1,51 @@
+from socketserver import ThreadingTCPServer,BaseRequestHandler
+import socket
+from sys import argv, exit
+
+class myHandler(BaseRequestHandler):
+    SOCKETS_LIST = []
+
+    def broadcast_string(self, b_msg, skip_socket):
+        for socket in self.SOCKETS_LIST:
+            if socket != myServer and socket != skip_socket:
+                socket.send(b_msg.encode())
+        print (b_msg)
+
+    def handle(self):
+        self.SOCKETS_LIST.append(self.request)
+        self.request.send(b"You're connected to the chatserver\r\n")
+        host, port = self.client_address
+        msg = "Client joined " + str(self.client_address) + "\r\n"
+        self.broadcast_string(msg,self.request)
+        while True:
+            data = self.request.recv(1024)
+            if data.decode() == "bye\r\n":
+                msg = "Client left" + str(self.client_address) + "\r\n"
+                self.SOCKETS_LIST.remove(self.request)
+                self.broadcast_string(msg,self.request)
+                self.print_sockets_list()
+                break
+            elif data.decode().startswith('s=>'):
+                msg = "[%s:%s] %s" % (host, port, data.decode())
+                print(msg)
+            else:
+                msg = "[%s:%s] %s" % (host, port, data.decode())
+                self.broadcast_string(msg, self.request)
+                self.print_sockets_list()
+    
+    def print_sockets_list(self):
+        print(self.SOCKETS_LIST)
+
+def main():
+    if len(argv) != 2:
+        print("[!] Use: MyChatRoom.py [Port]")
+        exit(1)
+    port = int(argv[1])
+    global myServer
+    myServer = ThreadingTCPServer(('localhost',port),myHandler)
+    myHandler.SOCKETS_LIST.append(myServer)
+    print ("ChatServer started on port %s" % port)
+    myServer.serve_forever()
+
+if __name__ == '__main__':
+    main()
---
+++ SocketThreadingServerTCP+.py
@@ -0,0 +1,22 @@
+from ast import arg
+from socketserver import ThreadingTCPServer, BaseRequestHandler
+from sys import argv, exit
+
+class myHandler(BaseRequestHandler):
+    def handle(self):
+        print ("Connection from ", str(self.client_address))
+        while True:
+            data = self.request.recv(1024)
+            if data.decode() == "bye\r\n": break
+            self.request.send(data.upper())
+        self.request.close()
+
+def main():
+    if len(argv) != 2:
+        print("[!] Use: SocketThreadingServerTCP+.py [Port]")
+        exit(1)
+    myServer = ThreadingTCPServer(("127.0.0.1", int(argv[1])), myHandler)
+    myServer.serve_forever()
+
+if __name__ == '__main__':
+    main()
