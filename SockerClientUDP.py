a2baf4a1b09f5b0498508b510e7c8de6895b3f78
Camilo Hernández Ruiz <camihruiz24@gmail.com>
2022-09-07 20:29:18 -0500

    Delete SockerClientUDP.py

--- SockerClientUDP.py
+++
@@ -1,8 +0,0 @@
-from socket import *
-c = socket (AF_INET,SOCK_DGRAM)
-entrada = input("Request> ")
-c.sendto(entrada.encode(),("192.168.30.72",4444))
-data,remote_host = c.recvfrom(1024)
-print(data.decode())
-c.close()
-exit()
